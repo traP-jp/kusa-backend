0d62c59255c9ffebcb9bf1a656d3d7f56530ea95
ramdos0207 <dev@ramdos.net>
2024-06-15 09:51:28 +0900

    initial commit

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `TASKS` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `content` TEXT NOT NULL,
+    `yomi` TEXT NOT NULL,
+    `iconUri` TEXT NOT NULL,
+    `authorDisplayName` TEXT NOT NULL,
+    `grade` TEXT NOT NULL,
+    `authorName` TEXT NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    `kusaCount` INT(11) NOT NULL,
+    `level` INT(11) NOT NULL,
+    `isSensitive` BOOLEAN NOT NULL,
+) ENGINE=INNODB DEFAULT CHARSET=UTF8;
